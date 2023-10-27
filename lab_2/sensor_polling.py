eac3ba925b4e062934c4eacde544a94e419ffb36
poe125 <rinbear98@gmail.com>
2023-10-27 14:17:32 -0700

    updating methods

--- lab_2/sensor_polling.py
+++ lab_2/sensor_polling.py
@@ -1,34 +1,42 @@
 import time
 import board
 import busio
+
+from adafruit_seesaw.seesaw import Seesaw
 import adafruit_sht31d
-import adafruit_seesaw.seesaw 
-import Seesaw
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-import neopixel
+
 import analogio
-from simpleio import map_range
+import simpleio
+
 from datetime import datetime
 from datetime import date
 
-#getting information from the ADS1015 12-bit ADC
-ads = ADS.ADS1015(i2c)
-chan = AnalogIn(ads, ADS.P0)
+#SoilMoisture.py
+def SoilMoisture():
+    i2c = board.I2C
+    ss = Seesaw(i2c, addr=0x36)
+    temperature = ss.get_temp()
+    moisture = ss.moisture_read()
 
 #getting information from SHT30 Temperature/Humidity sensor
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_sht31d.SHT31D(i2c)
-ss = Seesaw(i2c, addr=0x36)
+def TempHumidity():
+    i2cTH = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_sht31d.SHT31D(i2cTH)
+
+#getting information from the ADS1015 12-bit ADC
+def ADS():
+    i2cADS = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1015(i2cADS)
+    chan = AnalogIn(ads, ADS.P0)
 
 #getting time and date
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-current_date = now.strftime("%Y-%m-%d")
+def DateTime():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    current_date = now.strftime("%Y-%m-%d")
 
-# read temperature from the temperature sensor
-temperature = ss.get_temp()
-moisture = ss.moisture_read()
 
 #wind speed
 adc = analogio.AnalogIn(board.D4)
@@ -51,8 +59,8 @@ def main():
     print(current_date, current_time)
     
     #temperatue and etc.
-    print('Temperature: {0}C'.format(sensor.temperature))
     print('Humidity: {0}%'.format(sensor.relative_humidity))
+    print('Temperature: {0}C'.format(sensor.temperature))
     print('Soil Moisture:'+ str(moisture))
     print('Soil Temperature:'+ str(temperature))
     print('Wind Speed:' + str(wind_speed))
@@ -61,4 +69,9 @@ def main():
 #print data
 name()
 while(i2c):
+    SoilMoisture()
+    TempHumidity()
+    ADS()
+    DateTime()
+    
     main()
