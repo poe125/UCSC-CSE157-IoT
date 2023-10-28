46f44fea69aacb884e4ae1dfccbb6795dd5eb104
poe125 <rinbear98@gmail.com>
2023-10-27 18:38:17 -0700

    Completing polling-log.txt output (need formatting)

--- lab_2/sensor_polling.py
+++ lab_2/sensor_polling.py
@@ -20,46 +20,54 @@ def SoilMoisture():
     ss = Seesaw(i2c, addr=0x36)
     temperature = ss.get_temp()
     moisture = ss.moisture_read()
-    print('Soil Moisture:'+ str(moisture))
-    print('Soil Temperature:'+ str(temperature))
-    
+    soilMoisture = "Soil Moisture:" + str(moisture) + "\n Soil Temperature" + str(temperature) + "\n"
+#     print('Soil Moisture:'+ str(moisture))
+#     print('Soil Temperature:'+ str(temperature))
+    return soilMoisture
+
 #getting information from SHT30 Temperature/Humidity sensor
 def TempHumidity():
     sensor = adafruit_sht31d.SHT31D(I2C)
-    print('Humidity: {0}%'.format(sensor.relative_humidity))
-    print('Temperature: {0}C'.format(sensor.temperature))
-    
+#     print('Humidity: {0}%'.format(sensor.relative_humidity))
+#     print('Temperature: {0}C'.format(sensor.temperature))
+    tempHumidity = "Temperature:" + str(sensor.temperature) + "\n" + "Humidity" + str(sensor.relative_humidity) + "\n"    
+    return tempHumidity
+
 #getting information from the ADS1015 12-bit ADC
 def WindSpeed():
      ads = ADS.ADS1015(I2C)
      chan = AnalogIn(ads, ADS.P0)
-     max_number = 0;
-     print("ADS:",chan.value, chan.voltage)
-     while True:
-         voltage = chan.voltage
-         wind_speed = simpleio.map_range(voltage,	 #Actual read value
-                                      0.40, 0.78, #sensor min and max 
-                                      0, 32) #map min and max 
-#          if(max_number < voltage):
-#              max_number = voltage
-         print('Wind Speed:' + str(wind_speed)+ ' max:' + str(max_number) + " volt: " + str(voltage))
-     
+     map_speed = simpleio.map_range(chan.voltage, 0.40, 0.78, 0, 32) #map min and max 
+     #print('Wind Speed:' + str(wind_speed))
+     windSpeed = str(map_speed)
+     return windSpeed
+    
 #getting time and date
 def DateTime():
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     current_date = now.strftime("%Y-%m-%d")
-    print(current_date, current_time)
-
-#only once
-def name():
-    print("Airi Kokuryo")
+    dateTime = current_date + "\n" + current_time + "\n"
+    return dateTime
 
 def main():   
-    DateTime()
-   # SoilMoisture()
-    TempHumidity()
-    WindSpeed()
-    time.sleep(5)
+    with open('/home/akokuryo/Documents/polling-log.txt', 'w') as f:
+        f.write("AiriKokuryo")
     
+    while True:
+        print("write start")
+        returnDateTime = DateTime()
+        returnSoilMoisture = SoilMoisture()
+        returnTempHumidity = TempHumidity()
+        returnWindSpeed = WindSpeed()       
+        time.sleep(5)
+        print("complete")
+        
+        with open('/home/akokuryo/Documents/polling-log.txt', 'a') as f:
+            f.write(returnDateTime)
+            f.write(returnSoilMoisture)
+            f.write(returnTempHumidity)
+            f.write(returnWindSpeed)
+            f.write("\n")
+
 main()
